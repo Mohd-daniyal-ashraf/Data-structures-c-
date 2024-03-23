71d03d1e0a9549da6a7aa02355952c929a9fa715
Mohd daniyal ashraf <105990601+DANIYAL088@users.noreply.github.com>
2024-03-23 17:25:12 +0530

    Update mainlist.cpp

--- mainlist.cpp
+++ mainlist.cpp
@@ -27,3 +27,27 @@ int main(){
     // cout<<l.recsearch(key);
     
 }
+/*
+**************************************************************
+                            SECOND WAY
+**************************************************************
+*/
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+int main() {
+    Node* head = new Node(1);
+    head->next = new Node(2);
+    head->next->next = new Node(3);
+    head->next->next->next = new Node(4);
+    head->next->next->next->next = new Node(5);
+    return 0;
+}
